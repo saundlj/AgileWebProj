0d083c94c48bd0de8fcd3ea6e5e3c950eaa15c9a
mjb44552 <m50booth@gmail.com>
2024-04-23 16:08:38 +0800

    Updated Flask setup and configuration to resemble setup show in the Lab 07 Mega Tutorial. Added buttons to the login and create account pages.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from config import Config
 
 flaskApp = Flask(__name__)
+flaskApp.config.from_object(Config)
 
 from app import routes 
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,20 @@
 from app import flaskApp
+from flask import render_template,redirect, url_for
+from app.forms import CreateAccountForm, loginReturn
+
+@flaskApp.route('/login', methods = ['GET','POST'])
+def login():
+    form = CreateAccountForm()
+    if form.validate_on_submit():
+        return redirect(url_for('createAccount'))
+    return render_template("LoginPage.html",form = form)
+
+@flaskApp.route('/CreateAccount', methods = ['GET','POST'])
+def createAccount():
+    form = loginReturn()
+    if form.validate_on_submit():
+        return redirect(url_for('login'))
+    return render_template("CreateAccount.html", form = form)
 
 @flaskApp.route("/")
 def groups():
