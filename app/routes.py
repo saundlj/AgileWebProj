71c5bb5000a7ea2b401e8d679534112198a4f299
saundlj <98625810+saundlj@users.noreply.github.com>
2024-04-23 22:01:07 +0800

    - created some empty pages to learn github requests and test flask (AboutPage, FeedPage) - Added some dummy data to routes.py and played around with some ginga templates

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,28 @@ from app import flaskApp
 from flask import render_template,redirect, url_for
 from app.forms import CreateAccountForm, loginReturn
 
+posts = [
+    {
+    "title": "Software Engineer",
+    "date_posted": "April 23, 2024",
+    "company": "Apple",
+    "location": "San Francisco, CA",
+    "job_type": "Full-time",
+    "salary": "$90,000 - $120,000 per year",
+    "description": "Tech Innovations Inc. is seeking a talented and motivated Software Engineer to join our dynamic team in San Francisco. The successful candidate will be responsible for developing and maintaining high-quality software solutions, participating in the full software development lifecycle, and collaborating with cross-functional teams to deliver innovative products."
+    },
+
+    {
+    "title": "University Lecturer in Computer Science",
+    "date_posted": "April 22, 2024",
+    "company": "Tech University",
+    "location": "New York, NY",
+    "job_type": "Full-time, Tenure-track",
+    "salary": "Commensurate with experience",
+    "description": "Tech University's Department of Computer Science is seeking a dedicated and enthusiastic individual to join our faculty as a full-time, tenure-track University Lecturer. The successful candidate will contribute to the department's mission of providing high-quality education in computer science, conducting research, and engaging in service activities."
+    }
+]
+
 @flaskApp.route('/login', methods = ['GET','POST'])
 def login():
     form = CreateAccountForm()
@@ -18,5 +40,13 @@ def createAccount():
 
 @flaskApp.route("/")
 def groups():
-    return "Test Page. Flask is working"
+    return "Test Page. Flask is working... or is it. Maybe now?"
 
+
+@flaskApp.route("/about")
+def about():
+    return render_template("AboutPage.html")
+
+@flaskApp.route("/feed")
+def feed():
+    return render_template("FeedPage.html", posts = posts, title = 'Feed')
