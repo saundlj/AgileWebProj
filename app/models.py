d0a88226cf549b94164f0b9dae6e6c89a32c3f74
mjb44552 <m50booth@gmail.com>
2024-04-29 14:46:05 +0800

    Attempting to import required packages

---
+++ app/models.py
@@ -0,0 +1,18 @@
+from typing import Optional
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import db
+
+class User(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
+                                                unique=True)
+    email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True,
+                                             unique=True)
+    password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+
+    posts: so.WriteOnlyMapped['Post'] = so.relationship(
+        back_populates='author')
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
