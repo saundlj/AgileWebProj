0c917eb71153f87fb6dfabd373ef2c2ab7dadc82
saundlj <98625810+saundlj@users.noreply.github.com>
2024-05-18 21:01:58 +0800

    selenium changes... not working on windows laptop

--- tests/selenium.py
+++ tests/selenium.py
@@ -19,34 +19,18 @@ localHost = "http://localhost:5000/"
 class SeliniumTestCase(TestCase):
 
     def setUp(self):
-        # self.testApp = create_app(TestConfig)
-        # self.app_context = self.testApp.app_context()
-        # self.app_context.push()
-        # db.drop_all()
-        # db.create_all()
-        # add_test_users_to_db()
-
-        # # server responds to text
-        # # self.server_process = multiprocessing.Process(target=self.testApp.run)
-        # # self.server_process.start()
-        # self.server_thread = Thread(target=self.testApp.run, kwargs=localHost)
-        # self.server_thread.start()
-
-
-        # self.driver = webdriver.Chrome()
-        # self.driver.get(localHost)
-
         self.testApp = create_app(TestConfig)
         self.app_context = self.testApp.app_context()
         self.app_context.push()
-        db.drop_all()
         db.create_all()
         add_test_users_to_db()
 
         # Start the Flask server in a separate thread
-        self.server_thread = Thread(target=self.testApp.run, kwargs={'port': 5000})
-        self.server_thread.daemon = True  # Ensures the thread will be killed once the main program exits
-        self.server_thread.start()
+        # self.server_process = Thread(target=self.testApp.run)
+        # self.server_process.start()
+
+        self.server_process = multiprocessing.Process(target=self.testApp.run)
+        self.server_process.start()
 
         # Wait a bit to ensure the server has started
         time.sleep(2)
@@ -58,28 +42,20 @@ class SeliniumTestCase(TestCase):
 
 
     def tearDown(self):
-        # db.session.remove()
-        # db.drop_all()
-        # self.app_context.pop()
-
-        # # self.server_process.terminate()
-        # # self.driver.close()
-
-        # self.driver.quit()
-        # # Stop the Flask server
-        # self.server_thread.join()
-        self.driver.quit()
-
-        # Flask doesn't provide a built-in way to stop the server when running in a thread
-        # Thus, we manually stop it if necessary
-        if self.server_thread.is_alive():
-            self.server_thread._stop()
-
         db.session.remove()
         db.drop_all()
         self.app_context.pop()
 
+        # self.driver.quit()
+        # self.server_process.join()
+        # self.driver.close()
+
+        self.server_process.terminate()
+        self.driver.close()
+
+    
+
     def test_group_page(self):
-        time.sleep(10)
+        time.sleep(60)
         self.assertTrue(True)
     
