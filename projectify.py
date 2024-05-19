d5e43f274dbda1898bd9b5bf90d7c7d2addc13fb
saundlj <98625810+saundlj@users.noreply.github.com>
2024-05-19 16:37:18 +0800

    removed debug

--- projectify.py
+++ projectify.py
@@ -5,7 +5,5 @@ from app.config import DeploymentConfig
 if __name__ == '__main__':
     flaskApp = create_app(DeploymentConfig)
     migrate = Migrate(flaskApp, db) # test should also be updated with new schema 
-# # db.drop_all()
-# # db.create_all()
-# # import test_data
-    flaskApp.run(debug=True) 
+    # flaskApp.run(debug=True) 
+    flaskApp.run() 
